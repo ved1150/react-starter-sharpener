c0bd94e47e5f56af9268231f0a2b26524576b0c0
ved1150 <vppatel1771@gmail.com>
2022-09-13 20:33:30 +0530

    2nd action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import Expense from "./Expense";
 import Header from "./Header";
 function App() {
+  let arr =[
+    {
+      title : "burger" ,
+      amount: 200,
+      location: "mumbai" 
+    },
+    {
+      title : "ticket"  ,
+      amount: 1990,
+      location: "patna" 
+    },
+    {
+      title : "hotel" ,
+      amount: 20000,
+      location: "ahmedabad" 
+    },
+    {
+      title : "game" ,
+      amount: 2000,
+      location: "surat" 
+    }
+  ]
   return (
     <>
       <div className="App">
         <h1>ved</h1>
-        <Expense />
+        <div>{
+            arr.map((item) => {
+           return <Expense title={item.title} amount={item.amount} location={item.location}/>
+            })
+            }
+            </div>
         <Header />
       </div>
     </>
--- src/Expense.js
+++ src/Expense.js
@@ -1,7 +1,10 @@
-export default function Expense(){
+export default function Expense(props){
+
     return(
-       <>
-        <h1> Expense traker</h1>
-      </>
+      <div>
+        <h3>{props.title}</h3>
+        <h3>${props.amount}</h3>
+        <h3>{props.location}</h3>
+      </div>
     )
 }
