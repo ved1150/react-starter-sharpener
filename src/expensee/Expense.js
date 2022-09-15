3e89e4c407c5d002d3242d0d9efdd39feed3f2e6
ved1150 <vppatel1771@gmail.com>
2022-09-15 11:49:42 +0530

    change amount

--- src/expensee/Expense.js
+++ src/expensee/Expense.js
@@ -1,11 +1,17 @@
+ import React , { useState } from "react";
+
  let Expense= (props) =>{
-    let deleteHandler =() => {
-      console.log('click')
+    const [amount,setAmount] = useState(props.amount)
+
+    function deleteHandler(){
+      setAmount(100)
+      console.log(amount)
+      
     }
     return(
       <div>
         <h3>{props.title}</h3>
-        <h3>${props.amount}</h3>
+        <h3>${amount}</h3>
         <h3>{props.location}</h3>
         <button onClick={deleteHandler}>delete</button>
       </div>
