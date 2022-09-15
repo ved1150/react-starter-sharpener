84d8977d885e5b249870deb6ea0cd8469db99a8f
ved1150 <vppatel1771@gmail.com>
2022-09-15 08:27:38 +0530

    add delete button

--- src/expensee/Expense.js
+++ src/expensee/Expense.js
@@ -1,10 +1,13 @@
  let Expense= (props) =>{
-
+    let deleteHandler =() => {
+      console.log('click')
+    }
     return(
       <div>
         <h3>{props.title}</h3>
         <h3>${props.amount}</h3>
         <h3>{props.location}</h3>
+        <button onClick={deleteHandler}>delete</button>
       </div>
     )
 }
